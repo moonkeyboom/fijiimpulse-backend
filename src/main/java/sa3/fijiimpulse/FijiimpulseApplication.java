cb705e937fc627fe9be587e2196d9547ed3f2839
Ratanakamon Kaewmart <ratanakamon.k@ku.th>
2025-08-04 14:49:24 +0700

    first commit

---
+++ src/main/java/sa3/fijiimpulse/FijiimpulseApplication.java
@@ -0,0 +1,13 @@
+package sa3.fijiimpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FijiimpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FijiimpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa3/fijiimpulse/FijiimpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package sa3.fijiimpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FijiimpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
