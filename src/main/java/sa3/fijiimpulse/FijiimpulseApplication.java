e37b79570560d1391099b75ad75d48060b46b77d
Ratanakamon Kaewmart <ratanakamon.k@ku.th>
2025-08-21 12:48:15 +0700

    test main

--- src/main/java/sa3/fijiimpulse/FijiimpulseApplication.java
+++ src/main/java/sa3/fijiimpulse/FijiimpulseApplication.java
@@ -4,11 +4,63 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import sa3.fijiimpulse.entity.User;
+import sa3.fijiimpulse.repositories.UserDAO;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FijiimpulseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FijiimpulseApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(UserDAO dao) {
+		return args -> {
+//			System.out.println("Welcome to Fiji Impulse");
+//			saveData(dao);
+//			deleteData(dao);
+//			getData(dao);
+//			getAllUsers(dao);
+//			updateData(dao);
+		};
+	}
+
+	public void saveData(UserDAO dao){
+		User obj1 = new User("hiw", "hiw@gmail.com", "passwd", "customer");
+		dao.save(obj1);
+		System.out.println("User saved");
+	}
+
+	public void deleteData(UserDAO dao){
+		int userId = 4;
+		dao.delete(userId);
+		System.out.println("User deleted");
+	}
+
+	public void getData(UserDAO dao){
+		int userId = 1;
+		User user = dao.getUserById(userId);
+		System.out.println(user);
+	}
+
+	public void getAllUsers(UserDAO dao) {
+		List<User> users = dao.getAllUsers();
+		for (User user : users) {
+			System.out.println(user);
+		}
+	}
+
+	public void updateData(UserDAO dao) {
+		int userId = 6;
+		User user = dao.getUserById(userId);
+		System.out.println(user);
+		user.setUsername("mai");
+		user.setEmail("mai@gmail.com");
+		dao.updateUser(user);
+		System.out.println("User updated");
+		System.out.println(user);
 	}
 }
